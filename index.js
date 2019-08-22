a2009b6a6a7a5ab72553f73e4f21d326a788eb3e
ilya <ilya.soloveyv@gmail.com>
2019-08-22 15:36:15 +0300

    fix

--- index.js
+++ index.js
@@ -7,40 +7,14 @@ const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true })
 
 const bodyParser = require('body-parser')
-const fs = require('fs')
-const youtubedl = require('youtube-dl')
-const path = require('path')
-const sec = require('sec')
-// const Sequelize = require('sequelize')
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//   host: process.env.DB_HOST,
-//   dialect: 'mysql'
-// })
-
-// var Video = require('./models').video
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// const tempVideo = path.join(__dirname, 'upload/myvideo.mp4')
-// var tempVideoData = {}
-// var videoId = null
-
-// const Simons = require('./simons')(bot)
 const Simons = require('./simons')(bot)
 
 if (process.env.NODE_ENV == 'development') {
   Simons.getYoutube()
-  // const getInfo = async () => {
-  //   var temp = 
-  //   console.log('in function', temp)
-  //   return temp;
-  //  }
-   
-  //  getInfo()
-  
-  // console.log('index.js', )
-  // Simons()
 } else {
   new CronJob(process.env.CRON_TIME, function () {
     Simons.getYoutube()
--- simons.js
+++ simons.js
@@ -1,10 +1,21 @@
 const fetchUrl = require("fetch").fetchUrl
+const youtubedl = require('youtube-dl')
+const fs = require('fs')
+const path = require('path')
+const sec = require('sec')
+
+
 
 module.exports = function (bot) {
   var module = {}
   var Video = require('./models').video
   let YT_KEY = process.env.YT_KEY
   let YT_CHANNEL = process.env.YT_CHANNEL
+
+  let tempVideo = path.join(__dirname, 'upload/myvideo.mp4')
+  let tempVideoData = {}
+  let videoId = null
+
 
   module.sendMessage = () => {
     bot.sendMessage(process.env.TELEGRAM_ADMIN, 'sendMessage')
@@ -28,77 +39,59 @@ module.exports = function (bot) {
       var data = JSON.parse(body)
       let videoId = data.items[0].id.videoId
 
-      bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Video ID: ' + videoId)
-      
+      // bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Video ID: ' + videoId)
+      Video.count({
+        where: {
+          yt_video: videoId
+        }
+      }).then((count) => {
+        if (count == 0) {
+          var video = youtubedl('http://www.youtube.com/watch?v=' + videoId, ['--format=18'], {
+            cwd: __dirname
+          })
+          video.on('info', function (info) {
+            tempVideoData = info
+          })
+          video.on('end', () => {
+            bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Файл ' + videoId + ' успешно загружен на сервер')
+            Video.create({
+              yt_channel: process.env.YT_CHANNEL,
+              dateCreate: new Date(),
+              yt_video: videoId
+            }).then(() => {
+              if (fs.existsSync(tempVideo)) {
+                if (tempVideoData.filesize < 50000000) {
+                  bot.sendVideo(process.env.TELEGRAM_CHANNEL, tempVideo, {
+                    width: tempVideoData.width,
+                    height: tempVideoData.height,
+                    file_size: tempVideoData.filesize,
+                    caption: tempVideoData.title,
+                    duration: sec(tempVideoData.duration)
+                  }).then((data) => {
+                    bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Файл ' + videoId + ' размещен на канале')
+                    fs.unlink(tempVideo, (err) => {
+                      bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Временный файл ' + videoId + ' удален с сервера')
+                    })
+                  }, error => {
+                    bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Ошибка загрузки видео ' + videoId + ' на канал')
+                  })
+                } else {
+                  bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Файл ' + videoId + ' превысил 50Мб')
+                  fs.unlink(tempVideo, (err) => {
+                    bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Временный файл ' + videoId + ' удален с сервера')
+                  })
+                }
+              } else {
+                bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Файл ' + videoId + ' не найден на сервере')
+              }
+            })
+          })
+          video.pipe(fs.createWriteStream(tempVideo))
+        }
+      })
       console.log(videoId)
       return videoId;
     })
   }
-
   return module
-  
-  //   var data = JSON.parse(body)
-  //   if (!error) {
-      // bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Запущено без ошибок')
-    //   if (meta.status == 200) {
-    //     videoId = data.items[0].id.videoId
-    //     console.log(videoId)
-    //     Video.count({
-    //       where: {
-    //         yt_video: videoId
-    //       }
-    //     }).then((count) => {
-    //       if (count == 0) {
-    //         var video = youtubedl('http://www.youtube.com/watch?v=' + videoId, ['--format=18'], {
-    //           cwd: __dirname
-    //         })
-    //         video.on('info', function (info) {
-    //           tempVideoData = info
-    //         })
-    //         video.on('end', () => {
-    //           bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Файл успешно загружен на сервер')
-    //           Video.create({
-    //             yt_channel: process.env.YT_CHANNEL,
-    //             dateCreate: new Date(),
-    //             yt_video: videoId
-    //           }).then(() => {
-    //             if (fs.existsSync(tempVideo)) {
-    //               if (tempVideoData.filesize < 50000000) {
-    //                 bot.sendVideo(process.env.TELEGRAM_CHANNEL, tempVideo, {
-    //                   width: tempVideoData.width,
-    //                   height: tempVideoData.height,
-    //                   file_size: tempVideoData.filesize,
-    //                   caption: tempVideoData.title,
-    //                   duration: sec(tempVideoData.duration)
-    //                 }).then((data) => {
-    //                   bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Файл размещен на канале')
-    //                   fs.unlink(tempVideo, (err) => {
-    //                     bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Временный файл удален с сервера')
-    //                   })
-    //                 }, error => {
-    //                   bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Ошибка загрузки видео на канал')
-    //                 })
-    //               } else {
-    //                 bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Файл превысил 50Мб')
-    //                 fs.unlink(tempVideo, (err) => {
-    //                   bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Временный файл удален с сервера')
-    //                 })
-    //               }
-    //             } else {
-    //               bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Файл не найден на сервере')
-    //             }
-    //           })
-    //         })
-    //         video.pipe(fs.createWriteStream(tempVideo))
-    //       }
-    //     })
-    //   } else {
-    //     bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Ошибка META STATUS')
-    //     console.log('status', meta.status)
-    //   }
-  //   } else {
-  //     bot.sendMessage(process.env.TELEGRAM_ADMIN, 'Ошибка обращения к Google API')
-  //   }
-  // })
-  // return true
 }
